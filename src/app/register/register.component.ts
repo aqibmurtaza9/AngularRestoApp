c8710b3d9910b448dbcb15184b66308899a8a786
aqibmurtaza9 <aqibmurtaza9@gmail.com>
2021-11-10 17:57:03 +0500

    worked on register component

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { RestoService } from '../resto.service';
 
 @Component({
   selector: 'app-register',
@@ -6,10 +8,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
-
-  constructor() { }
+  alert: boolean = false;
+  constructor(private resto:RestoService) { }
+  register = new FormGroup({
+    name: new FormControl(''),
+    address: new FormControl(''),
+    email: new FormControl('')
+  })
 
   ngOnInit(): void {
+  }
+ 
+  collection(){
+    console.warn(this.register.value);
+    this.resto.registerUser(this.register.value).subscribe((result)=>{
+       console.warn(result);
+       this.alert=true;
+       this.register.reset({})
+    })
+  }
+  closeAlert(){
+    this.alert=false;
   }
 
 }
--- src/app/resto.service.ts
+++ src/app/resto.service.ts
@@ -1,32 +1,34 @@
 import { Injectable } from '@angular/core';
-import{HttpClient} from'@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RestoService {
-url="http://localhost:3000/restaurants";
+  url = "http://localhost:3000/restaurants";
+  registerURL="http://localhost:3000/";
 
+  constructor(private http: HttpClient) { }
 
-constructor( private http:HttpClient) { }
-
-  getList(){
+  getList() {
     return this.http.get(this.url);
   }
-  
-  saveResto(data:any){
-     return this.http.post(this.url,data)
+
+  saveResto(data: any) {
+    return this.http.post(this.url, data)
   }
 
-  delResto(id:any){
+  delResto(id: any) {
     return this.http.delete(`${this.url}/${id}`)
   }
-  
-  getCurrentResto(id:any){
+
+  getCurrentResto(id: any) {
     return this.http.get(`${this.url}/${id}`)
   }
-  updateResto(id:any,data:any){
-    return this.http.put(`${this.url}/${id}`,data)
+  updateResto(id: any, data: any) {
+    return this.http.put(`${this.url}/${id}`, data)
   }
-
+  registerUser(data:any){
+    return this.http.post(this.registerURL+"users",data)
+  }
 }
